7b06da4706f26073c8a361884204e0949e35aa20
quangvinh124 <53868788+nguyenquangvinh2000@users.noreply.github.com>
2020-02-14 10:48:21 +0700

    Create tienhoa.cpp

---
+++ tienhoa.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long n,sum=0;
+    cin>>n;
+    while(n>0){
+        sum=sum+n;
+        n=n/10;
+    }
+    cout<<sum;
+    return 0;
+}
